fa2418b413f3968da5b4389862dd2300e12bcfb9
Thomas Westhoff <th.westhoff@gmail.com>
2013-10-21 13:46:52 +0200

    Falscher <?php Eintrag ...

--- bootstrap/farbcode.php
+++ bootstrap/farbcode.php
@@ -8,7 +8,7 @@
 		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 	</head>
 	<body>
-	<?	$farbname=array('schwarz','braun','rot','orange','gelb','gr&uuml;n','blau','lila','grau','wei&szlig;'); 
+	<?php	$farbname=array('schwarz','braun','rot','orange','gelb','gr&uuml;n','blau','lila','grau','wei&szlig;'); 
 		$farbnummer=array('#000000','#996633','#FF0000','#FFA500','#FFFF00','#00FF00','#0000FF','#9900CC','#808080','#FFFFFF');
 		$vordergrund=array('#FFFFFF','#FFFFFF','#000000','#000000','#000000','#000000','#FFFFFF','#FFFFFF','#000000','#000000');
 		$toleranz=array('-','+- 1%','+- 2%','-','-','+- 0,5%','+- 0,25%','+- 0,1%','+- 0,05%','-');
@@ -20,13 +20,13 @@
 			<form name="eingabe" action="farbcode.php" method="post">
 				<h1>Widerstandsberechnung</h1>
 				<div class="row">
-					<?	for ($i=1;$i<=4;$i++) {
+					<?php	for ($i=1;$i<=4;$i++) {
 							echo '<div class="span3" style="background-color:'.$farbnummer[$ring[$i-1]].';color:'.$vordergrund[$ring[$i-1]].'"><h2>Ring '.$i.'</h2></div>'; }
 						$wert=($ring[0].$ring[1])*pow(10,$ring[2]);
 						$wert=$wert." &Omega; ".$toleranz[$ring[3]];?>
 				</div>
 				<div class="row">
-					<?	for($i=1;$i<=4;$i++) { 
+					<?php	for($i=1;$i<=4;$i++) { 
 							echo '<div class="span3"><select name="ring[]" onChange="javascript:document.eingabe.submit()">';
 							for ($j=0;$j<=9;$j++) {
 								($ring[$i-1]==$j) ? $sel="selected" : $sel="";
@@ -34,7 +34,7 @@
 							echo '</select></div>';
 						}?>	
 						</div>
-				<? echo '<h2>Widerstandswert: '.$wert.'</h2>'; ?>
+				<?php echo '<h2>Widerstandswert: '.$wert.'</h2>'; ?>
 			</form>
 		</div>
 		<script src="http://code.jquery.com/jquery.js"></script>
